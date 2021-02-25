27b5acffe94ac4dfe4051d28df2aa09a01288a61
LeadHUsp <bodvinukr@gmail.com>
2021-02-25 22:17:34 +0300

    home tabs added

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const { src, dest, series, parallel, watch } = require("gulp");
-const pug = require("gulp-pug");
-const sass = require("gulp-sass");
-const notify = require("gulp-notify");
-const concat = require("gulp-concat");
-const rename = require("gulp-rename");
-const imagemin = require("gulp-imagemin");
-const cleanCSS = require("gulp-clean-css");
-const uglify = require("gulp-uglify-es").default;
-const sourcemaps = require("gulp-sourcemaps");
-const formatHtml = require("gulp-format-html");
-const autoprefixer = require("gulp-autoprefixer");
-const fileInclude = require("gulp-file-include");
-const del = require("del");
-const webpack = require("webpack");
-const browserSync = require("browser-sync").create();
-const pngquant = require("imagemin-pngquant");
-const webpackStream = require("webpack-stream");
+const { src, dest, series, parallel, watch } = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require('gulp-sourcemaps');
+const formatHtml = require('gulp-format-html');
+const autoprefixer = require('gulp-autoprefixer');
+const fileInclude = require('gulp-file-include');
+const del = require('del');
+const webpack = require('webpack');
+const browserSync = require('browser-sync').create();
+const pngquant = require('imagemin-pngquant');
+const webpackStream = require('webpack-stream');
 // const svgSprite     = require('gulp-svg-sprite');
 // const ttf2woff      = require('gulp-ttf2woff');
 // const ttf2woff2     = require('gulp-ttf2woff2');
@@ -24,62 +24,63 @@ const config = {
   isPug: true, // If true, the pugCompiller task is used, otherwise the html task is used
 };
 const path = {
-  app: "./app",
+  app: './app',
   html: {
-    html: "./src/*.html",
-    pug: ["./src/pug/*.pug", "./src/pug/blocks/*.pug"],
-    result: "./app/",
+    html: './src/*.html',
+    pug: ['./src/pug/*.pug', './src/pug/blocks/*.pug'],
+    pugBuild: './src/pug/*.pug',
+    result: './app/',
   },
   style: {
     libs: [
-      "./src/libs/Magnific-Popup-master/dist/magnific-popup.css",
-      "./src/libs/slick-carousel/slick/slick.css",
-      "./src/libs/stellarnav-master/css/stellarnav.min.css",
+      './src/libs/Magnific-Popup-master/dist/magnific-popup.css',
+      './src/libs/slick-carousel/slick/slick.css',
+      './src/libs/stellarnav-master/css/stellarnav.min.css',
     ],
-    scss: ["./src/scss/**/*.scss", "./src/scss/**/*.sass"],
-    result: "./app/css",
+    scss: ['./src/scss/**/*.scss', './src/scss/**/*.sass'],
+    result: './app/css',
   },
   images: {
     // svg: './src/img/**/*.svg',
     source: [
-      "./src/img/**/*.jpg",
-      "./src/img/**/*.jpeg",
-      "./src/img/**/*.png",
-      "./src/img/**/*.svg",
+      './src/img/**/*.jpg',
+      './src/img/**/*.jpeg',
+      './src/img/**/*.png',
+      './src/img/**/*.svg',
     ],
-    result: "./app/img",
+    result: './app/img',
   },
   fonts: {
-    allFons: "./src/fonts/**",
-    link: "./src/fonts/**/*.css",
-    source: "./src/fonts/**/*.ttf",
-    result: "./app/fonts",
+    allFons: './src/fonts/**',
+    link: './src/fonts/**/*.css',
+    source: './src/fonts/**/*.ttf',
+    result: './app/fonts',
   },
   scripts: {
     libs: [
-      "./src/libs/jquery/dist/jquery.min.js",
-      "./src/libs/jquery.cookie/jquery.cookie.js",
-      "./src/libs/Magnific-Popup-master/dist/jquery.magnific-popup.min.js",
-      "./src/libs/slick-carousel/slick/slick.min.js",
-      "./src/libs/stellarnav-master/js/stellarnav.min.js",
-      "./src/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js",
+      './src/libs/jquery/dist/jquery.min.js',
+      './src/libs/jquery.cookie/jquery.cookie.js',
+      './src/libs/Magnific-Popup-master/dist/jquery.magnific-popup.min.js',
+      './src/libs/slick-carousel/slick/slick.min.js',
+      './src/libs/stellarnav-master/js/stellarnav.min.js',
+      './src/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js',
     ],
     source: {
-      all: "./src/js/**/*.js",
-      index: "./src/js/index.js",
-      libs: "",
+      all: './src/js/**/*.js',
+      index: './src/js/index.js',
+      libs: '',
     },
-    result: "./app/js",
+    result: './app/js',
   },
-  resources: "./src/resources/**",
+  resources: './src/resources/**',
 };
 //HTML
 const html = () => {
   return src(path.html.html)
     .pipe(
       fileInclude({
-        prefix: "@",
-        basepath: "@file",
+        prefix: '@',
+        basepath: '@file',
       })
     )
     .pipe(dest(path.html.result))
@@ -92,24 +93,31 @@ const pugCompiller = () => {
     .pipe(dest(path.html.result))
     .pipe(browserSync.stream());
 };
+const pugCompillerBuild = () => {
+  return src(path.html.pugBuild)
+    .pipe(pug())
+    .pipe(formatHtml())
+    .pipe(dest(path.html.result))
+    .pipe(browserSync.stream());
+};
 //CSS
 const styles = () => {
   return src(path.style.scss)
     .pipe(sourcemaps.init())
     .pipe(
       sass({
-        outputStyle: "expanded",
-      }).on("error", notify.onError())
+        outputStyle: 'expanded',
+      }).on('error', notify.onError())
     )
     .pipe(
       rename({
-        suffix: ".min",
+        suffix: '.min',
       })
     )
     .pipe(
       autoprefixer({
-        overrideBrowserslist: ["last 2 versions"],
-        cascade: false,
+        cascade: true,
+        grid: 'autoplace',
       })
     )
     .pipe(
@@ -117,19 +125,19 @@ const styles = () => {
         level: 2,
       })
     )
-    .pipe(sourcemaps.write("."))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest(path.style.result))
     .pipe(browserSync.stream());
 };
 const syleLibs = () => {
   return src(path.style.libs)
-    .pipe(concat("libs.css"), { allowEmpty: true })
+    .pipe(concat('libs.css'), { allowEmpty: true })
     .pipe(
       rename({
-        suffix: ".min",
+        suffix: '.min',
       })
     )
-    .pipe(cleanCSS({ compatibility: "ie10" }))
+    .pipe(cleanCSS({ compatibility: 'ie10' }))
     .pipe(dest(path.style.result));
 };
 //IMAGES && SVG
@@ -166,8 +174,9 @@ const scripts = () => {
   return src(path.scripts.source.index)
     .pipe(
       webpackStream({
+        mode: 'development',
         output: {
-          filename: "index.js",
+          filename: 'index.js',
         },
         module: {
           rules: [
@@ -175,9 +184,9 @@ const scripts = () => {
               test: /\.m?js$/,
               exclude: /node_modules|bower_components/,
               use: {
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 options: {
-                  presets: [["@babel/preset-env", { targets: "defaults" }]],
+                  presets: [['@babel/preset-env', { targets: 'defaults' }]],
                 },
               },
             },
@@ -186,14 +195,14 @@ const scripts = () => {
       })
     )
     .pipe(sourcemaps.init())
-    .pipe(uglify().on("error", notify.onError()))
-    .pipe(sourcemaps.write("."))
+    .pipe(uglify().on('error', notify.onError()))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest(path.scripts.result))
     .pipe(browserSync.stream());
 };
 const scriptLibs = () => {
   return src(path.scripts.libs)
-    .pipe(concat("libs.js"), { allowEmpty: true })
+    .pipe(concat('libs.js'), { allowEmpty: true })
     .pipe(uglify())
     .pipe(dest(path.scripts.result));
 };
@@ -209,13 +218,11 @@ const resources = () => {
 const watchFiles = () => {
   browserSync.init({
     server: {
-      baseDir: "./app",
+      baseDir: './app',
     },
   });
   watch(path.style.scss, styles);
-  config.isPug
-    ? watch(path.html.pug, pugCompiller)
-    : watch(path.html.html, html);
+  config.isPug ? watch(path.html.pug, pugCompiller) : watch(path.html.html, html);
   watch(path.images.source, image);
   watch(path.fonts.source, fonts);
   watch(path.scripts.source.all, scripts);
@@ -240,18 +247,18 @@ const stylesBuild = () => {
   return src(path.style.scss)
     .pipe(
       sass({
-        outputStyle: "expanded",
-      }).on("error", notify.onError())
+        outputStyle: 'expanded',
+      }).on('error', notify.onError())
     )
     .pipe(
       rename({
-        suffix: ".min",
+        suffix: '.min',
       })
     )
     .pipe(
       autoprefixer({
-        overrideBrowserslist: ["last 2 versions"],
-        cascade: false,
+        cascade: true,
+        grid: 'autoplace',
       })
     )
     .pipe(
@@ -265,8 +272,9 @@ const scriptsBuild = () => {
   return src(path.scripts.source.index)
     .pipe(
       webpackStream({
+        mode: 'production',
         output: {
-          filename: "index.js",
+          filename: 'index.js',
         },
         module: {
           rules: [
@@ -274,9 +282,9 @@ const scriptsBuild = () => {
               test: /\.m?js$/,
               exclude: /node_modules|bower_components/,
               use: {
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 options: {
-                  presets: [["@babel/preset-env", { targets: "defaults" }]],
+                  presets: [['@babel/preset-env', { targets: 'defaults' }]],
                 },
               },
             },
@@ -284,7 +292,7 @@ const scriptsBuild = () => {
         },
       })
     )
-    .pipe(uglify().on("error", notify.onError()))
+    .pipe(uglify().on('error', notify.onError()))
     .pipe(dest(path.scripts.result));
 };
 const imageBuild = () => {
@@ -300,12 +308,12 @@ const imageBuild = () => {
     .pipe(dest(path.images.result));
 };
 const destination = () => {
-  return src(path.app + "/**/*").pipe(dest("./dist"));
+  return src(path.app + '/**/*').pipe(dest('./dist'));
 };
 exports.build = series(
   clean,
   parallel(
-    config.isPug ? pugCompiller : html,
+    config.isPug ? pugCompillerBuild : html,
     scriptLibs,
     scriptsBuild,
     fonts,
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,48 @@
 $(document).ready(function ($) {
-  $(".stellarnav").stellarNav({
+  $('.stellarnav').stellarNav({
     breakpoint: 1280,
   });
-  $("input[type='tel']").inputmask("+9(999)-999-99-99");
+  $("input[type='tel']").inputmask('+9(999)-999-99-99');
+  //tabs
+  let tab = (
+    controlSelector,
+    contentSelector,
+    controlActiveClass,
+    contentActiveClass
+  ) => {
+    const tabControl = document.querySelectorAll(controlSelector);
+    const tabContent = document.querySelectorAll(contentSelector);
+    let tabName;
+    tabControl.forEach((item) => {
+      item.addEventListener('click', selectNavTab);
+    });
+
+    function selectNavTab() {
+      tabControl.forEach((tab) => {
+        tab.classList.remove(controlActiveClass);
+      });
+      this.classList.add(controlActiveClass);
+      tabName = this.getAttribute('data-tab');
+      selectTabContent(tabName);
+    }
+    function selectTabContent(tabName) {
+      tabContent.forEach((item) => {
+        item.getAttribute('data-tab-content') == tabName
+          ? item.classList.add(contentActiveClass)
+          : item.classList.remove(contentActiveClass);
+      });
+    }
+  };
+  tab(
+    '.tabs-nav__elem',
+    '.tabs-content__item',
+    'tabs-nav__elem-active',
+    'tabs-content__item-active'
+  );
+  /*  tab(
+    '#profile-tabs .tab-control__btn',
+    '#profile-tabs .profile-tab',
+    'tab-control__btn-active-secondary',
+    'profile-tab-active'
+  ); */
 });
