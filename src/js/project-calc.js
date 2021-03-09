fdf3dd721f32611c35c3ca15a925f57d1c0c15ea
LeadHUsp <bodvinukr@gmail.com>
2021-03-09 21:54:11 +0300

    3d calc control added

--- src/js/project-calc.js
+++ src/js/project-calc.js
@@ -1,23 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const typeOfGlassNavEl = document.querySelectorAll(
-    ".project-calc__label-type-glass"
-  );
-  const typeOfLightNavEl = document.querySelectorAll(
-    '*[data-group="typeof-light"]'
-  );
-  const imgInsideEl = document.getElementById("project-calc__inside-img");
-  const imgLightEl = document.getElementById("project-calc__light-img");
+import { tab } from './tab';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const typeOfGlassNavEl = document.querySelectorAll('.project-calc__label-type-glass');
+  const typeOfLightNavEl = document.querySelectorAll('*[data-group="typeof-light"]');
+  const typeOfWarmingNavEl = document.querySelectorAll('*[data-group="typeof-warming"]');
+  const imgInsideEl = document.getElementById('project-calc__inside-img');
+  const imgLightEl = document.getElementById('project-calc__light-img');
+  const imgWarmingEl = document.getElementById('project-calc__warming-img');
   typeOfGlassNavEl.forEach((item) => {
-    item.addEventListener("click", () => {
-      let dataSrc = item.getAttribute("data-src");
-      imgInsideEl.setAttribute("src", dataSrc);
+    item.addEventListener('click', () => {
+      let dataSrc = item.getAttribute('data-src');
+      imgInsideEl.setAttribute('src', dataSrc);
     });
   });
   typeOfLightNavEl.forEach((item) => {
-    item.addEventListener("click", () => {
-      let dataSrc = item.getAttribute("data-src");
-      imgLightEl.setAttribute("src", dataSrc);
+    item.addEventListener('click', () => {
+      let dataSrc = item.getAttribute('data-src');
+      imgLightEl.setAttribute('src', dataSrc);
     });
   });
+  typeOfWarmingNavEl.forEach((item) => {
+    item.addEventListener('click', () => {
+      let dataSrc = item.getAttribute('data-src');
+      imgWarmingEl.setAttribute('src', dataSrc);
+    });
+  });
+  tab(
+    '.project-calc__tab-nav .project-calc__label',
+    '.project-calc__tab-content .project-calc__tab-item',
+    'child-tabs__btn-active',
+    'project-calc__tab-active',
+    false
+  );
 });
